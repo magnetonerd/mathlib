2dc34573eaf63f5e71d94c119962f3873817e779
magnetonerd <magnetonerd@gmail.com>
2016-12-10 08:56:03 -0600

    Update gaussquad.py

--- gaussquad.py
+++ gaussquad.py
@@ -21,7 +21,7 @@ def gaussxw(N):
         p1 = x #a copy of x 
         #Recussion definition of Legendre Polynomials
         for k in range(1,N):
-            p0,p1 = p1, ((2*k+1)*x*p1-k*p0)/(k+1) #this has to be done as one line. Two lines confudes interpreter
+            p0,p1 = p1, ((2*k+1)*x*p1-k*p0)/(k+1) #this has to be done as one line. Two lines confuses interpreter
         dP = (N+1)*(x*p1-p0)/(x**2-1) #Derivative of Legendre Polynomials with respect to x
         dx = p1/dP #Seperation between the initial geuss and and new point using newton;s method
         x -= dx #New geuss of location of root. Once loop finishes these are the sample points
